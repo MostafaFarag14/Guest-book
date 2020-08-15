c4432672f3f185050a13054f9a0d06b1221e79c6
mostafafarag <mostafafarag140@gmail.com>
2020-08-15 16:48:59 +0200

    Updates

---
+++ precache-manifest.82b00d47956d9a7670c305c5fcce911c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "82e7b7b6f46e68ba32b2b2e369948929",
+    "url": "/Guest-book/index.html"
+  },
+  {
+    "revision": "7329a1c1b5bcd4d63845",
+    "url": "/Guest-book/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "d8aa50796f6e6cdb6574",
+    "url": "/Guest-book/static/css/main.1161689f.chunk.css"
+  },
+  {
+    "revision": "7329a1c1b5bcd4d63845",
+    "url": "/Guest-book/static/js/2.e1fd78b1.chunk.js"
+  },
+  {
+    "revision": "56729d980da09961afe8d497a21eec12",
+    "url": "/Guest-book/static/js/2.e1fd78b1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d8aa50796f6e6cdb6574",
+    "url": "/Guest-book/static/js/main.b1411211.chunk.js"
+  },
+  {
+    "revision": "683702f2d65d9b13af56",
+    "url": "/Guest-book/static/js/runtime-main.bb38f13f.js"
+  }
+]);
